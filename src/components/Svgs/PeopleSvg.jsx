f9f7a54ee646d2299468ea04d6f3d42a79665c2e
Anghelo Alagon <200518@unsaac.edu.pe>
2024-08-25 22:28:14 -0500

    add svg for teams section

---
+++ src/components/Svgs/PeopleSvg.jsx
@@ -0,0 +1,15 @@
+export const PeopleSvg = ({ color }) => {
+  return (
+    <svg width="43" height="27" viewBox="0 0 43 27" fill="none" xmlns="http://www.w3.org/2000/svg" style={{ color: `#${color}` }} >
+      <rect x="0.75" y="0.852783" stroke="currentColor" width="42" height="26" rx="13" fill="currentColor" />
+      <g clipPath="url(#clip0_1252_626)">
+        <path stroke="currentColor" d="M13.0439 8.55898C12.6533 8.16836 12.6533 7.53711 13.0439 7.14648C13.4346 6.75586 14.0689 6.75586 14.4596 7.14648L20.4596 13.1465C20.8502 13.5371 20.8502 14.1715 20.4596 14.5621L14.4596 20.5621C14.0689 20.9527 13.4346 20.9527 13.0439 20.5621C12.6533 20.1715 12.6533 19.5371 13.0439 19.1465L18.3346 13.8527L13.0439 8.55898ZM20.7502 18.8527H29.7502C30.3033 18.8527 30.7502 19.2996 30.7502 19.8527C30.7502 20.4059 30.3033 20.8527 29.7502 20.8527H20.7502C20.1971 20.8527 19.7502 20.4059 19.7502 19.8527C19.7502 19.2996 20.1971 18.8527 20.7502 18.8527Z" fill="#0A0E12" />
+      </g>
+      <defs>
+        <clipPath id="clip0_1252_626">
+          <rect width="18" height="16" stroke="currentColor" fill="white" transform="translate(12.75 5.85278)" />
+        </clipPath>
+      </defs>
+    </svg>
+  )
+}
