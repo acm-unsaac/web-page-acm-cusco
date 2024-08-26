faf77251c566dfeff69d14b2e1797d6e14c60ded
Anghelo Alagon <200518@unsaac.edu.pe>
2024-08-25 22:34:40 -0500

    update tailwind config to add custom rotate states

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,10 @@ module.exports = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    rotate: {
+      '50': "-45deg",
+      '45': "45deg"
+    },
   },
   plugins: [],
 };
