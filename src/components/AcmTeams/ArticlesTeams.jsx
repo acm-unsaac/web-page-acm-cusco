42d42e70796b93388884a173f8abca3a742f0ba3
Anghelo Alagon <200518@unsaac.edu.pe>
2024-08-27 23:00:21 -0500

    add hover and change font family in articlesTeams

--- src/components/AcmTeams/ArticlesTeams.jsx
+++ src/components/AcmTeams/ArticlesTeams.jsx
@@ -4,7 +4,10 @@ import { TitleTeams } from "./TitleTeams"
 export const ArticlesTeams = ({ children, color, content, links }) => {
   const { title, info } = content
   return (
-    <article className="flex flex-col justify-between items-ceter pt-[33px]  bg-[#0F1317] shadow h-[245px] w-[336px] rounded-xl py-[16px] px-[30px]">
+    <article
+      className="acmTeams_hover font-Opensans text-white-azul flex flex-col justify-between items-ceter pt-[33px]  bg-[#0F1317] shadow h-[245px] w-[336px] rounded-xl py-[16px] px-[30px] relative drop-shadow-[0_0_0.3rem_#000307] transition-all duration-300"
+      style={{ '--hover-color': `#${color}` }}
+    >
       <TitleTeams color={color} title={title}>
         {children}
       </TitleTeams>
